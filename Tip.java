cfae6d48aafaa9a703cce5549405ceccf2e86e97
emoryLu <qianlu@tamu.edu>
2020-09-29 16:12:31 -0500

    Reverted changese.

--- Tip.java
+++ Tip.java
@@ -2,7 +2,7 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Tip{
-	public static void main(String[] args) {
+	public static void main(Connection conn) {
 		System.out.println("Tip entity currently not populated, so closing");
 	}
 }
