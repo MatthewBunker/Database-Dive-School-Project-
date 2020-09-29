dfb93ffa62407dc4b1f913326a91d4ff7a78625c
emoryLu <qianlu@tamu.edu>
2020-09-29 15:56:07 -0500

    Defined main method for Tip.java

--- Tip.java
+++ Tip.java
@@ -2,7 +2,7 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Tip{
-	public static void main(Connection conn) {
+	public static void main(String[] args) {
 		System.out.println("Tip entity currently not populated, so closing");
 	}
 }
