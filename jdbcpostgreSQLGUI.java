f78aace8606c68711ccb0148d24774d7058114c8
Tony Vo <tvo8275@tamu.edu>
2020-09-29 22:46:21 -0500

    Tidied code, and Added empty implementation of write to text file

--- jdbcpostgreSQLGUI.java
+++ jdbcpostgreSQLGUI.java
@@ -1,57 +1,12 @@
 import java.sql.*;
 import javax.swing.JOptionPane;
 import java.util.Scanner;
+import java.io.File;
+import java.io.IOException;
 //import java.sql.DriverManager;
 /*
 CSCE 315
 9-25-2019
- */
- 
- 
-/* public class jdbcpostgreSQLGUI {
-  public static void main(String args[]) {
-    dbSetup my = new dbSetup();
-    //Building the connection
-     Connection conn = null;
-     try {
-        Class.forName("org.postgresql.Driver");
-        conn = DriverManager.getConnection("jdbc:postgresql://csce-315-db.engr.tamu.edu/db911_group11_theintroverts",
-           my.user, my.pswd);
-     } catch (Exception e) {
-        e.printStackTrace();
-        System.err.println(e.getClass().getName()+": "+e.getMessage());
-        System.exit(0);
-     }//end try catch
-     JOptionPane.showMessageDialog(null,"Opened database successfully");
-     String cus_lname = "";
-     try{
-     //create a statement object
-       Statement stmt = conn.createStatement();
-       //create an SQL statement
-       String sqlStatement = "SELECT \"Name\" FROM \"User\" LIMIT 10";
-       //send statement to DBMS
-       ResultSet result = stmt.executeQuery(sqlStatement);
-
-       //OUTPUT
-       JOptionPane.showMessageDialog(null,"10 names from User");
-       //System.out.println("______________________________________");
-       while (result.next()) {
-         //System.out.println(result.getString("cus_lname"));
-         cus_lname += result.getString("Name")+"\n";
-       }
-   } catch (Exception e){
-     JOptionPane.showMessageDialog(null,"Error accessing Database.");
-   }
-   JOptionPane.showMessageDialog(null,cus_lname);
-    //closing the connection
-    try {
-      conn.close();
-      JOptionPane.showMessageDialog(null,"Connection Closed.");
-    } catch(Exception e) {
-      JOptionPane.showMessageDialog(null,"Connection NOT Closed.");
-    }//end try catch
-  }//end main
-}//end Class
  */
 
 public class jdbcpostgreSQLGUI {
@@ -70,24 +25,12 @@ public class jdbcpostgreSQLGUI {
         System.err.println(e.getClass().getName()+": "+e.getMessage());
         System.exit(0);
      }//end try catch
-     //System.out.println("Opened database successfully");
 	 JOptionPane.showMessageDialog(null,"Opened database successfully");
      String cus_lname = "";
      try{
 		boolean initPass = true;
 		while(initPass) {
-	   //Printing options for Users, changeable based on design
-		   /* System.out.println("0: Close");
-		   System.out.println("1: Checkin");
-		   System.out.println("2: Address");
-		   System.out.println("3: Parking");
-		   System.out.println("4: Business");
-		   System.out.println("5: Review (WIP)");
-		   System.out.println("6: User Compliments");
-		   System.out.println("7: Tip (WIP)");
-		   System.out.println("8: User");
-		   System.out.println("9: Hours"); */
-		   //Getting input from user
+		//Printing options for Users, changeable based on design
 		   String option = JOptionPane.showInputDialog(null,"0: Close\n"
 										    + "1: Checkin\n"
 											+ "2: Address\n"
@@ -99,20 +42,13 @@ public class jdbcpostgreSQLGUI {
 											+ "8: User\n"
 											+ "9: Hours\n"
 											+ "Input desired command (enter \"1\" for checkin): ");
-			//Scanner input = new Scanner(System.in);
-
-			//System.out.println("Input desired command (enter \"1\" for checkin): ");
-			//int option = input.nextInt();
-			//String option = JOptionPane.showInputDialog(null, "Input desired command (enter \"1\" for checkin): ");
+			int textOpt = JOptionPane.showConfirmDialog(null, "Print to a txt file?");
 			int optionInt = Integer.parseInt(option);
 			String business_name = "";
 			boolean name_bool = false;
 			String user_id = "";
 			boolean user_bool = false;
 			if(optionInt != 6 && optionInt != 8 && optionInt != 0){
-				//System.out.println("Enter desired business name or enter N/A");
-				//Scanner input2 = new Scanner(System.in);
-				//business_name = input2.nextLine();
 				business_name = JOptionPane.showInputDialog(null, "Enter desired business name or enter N/A\n"
 																+ "Examples: \"Burger King\" or \"Starbucks\"");
 				
@@ -124,9 +60,6 @@ public class jdbcpostgreSQLGUI {
 				}
 			}
 			else if(optionInt == 6 || optionInt == 8 && optionInt != 0){
-				//System.out.println("Enter desired user ID or enter N/A");
-				//Scanner input2 = new Scanner(System.in);
-				//user_id = input2.nextLine();
 				user_id = JOptionPane.showInputDialog(null, "Enter desired user ID or enter N/A\n"
 														  + "Examples: \"--2vR0DIsmQ6WfcSzKWigw\"");
 				if(user_id.equals("N/A")){
@@ -139,95 +72,69 @@ public class jdbcpostgreSQLGUI {
 	
 			switch(optionInt) {
 				case 0:
-					//System.out.println("Close selected");
 					JOptionPane.showMessageDialog(null,"Close selected");
 					initPass = false;
 					break;
 				case 1:
-					//System.out.println("Checkin selected");
 					JOptionPane.showMessageDialog(null,"Checkin selected");
 					initPass = false;
 					CheckIn.main(conn, business_name, name_bool);
 					break;
 				case 2:
-					//System.out.println("Address selected");
 					JOptionPane.showMessageDialog(null,"Address selected");
 					initPass = false;
 					Address.main(conn, business_name, name_bool);
 					break;
 				case 3:
-					//System.out.println("Parking selected");
 					JOptionPane.showMessageDialog(null,"Parking selected");
 					initPass = false;
 					Parking.main(conn, business_name, name_bool);
 					break;
 				case 4:
-					//System.out.println("Business selected");
 					JOptionPane.showMessageDialog(null,"Business selected");
 					initPass = false;
 					Business.main(conn, business_name, name_bool);
 					break;
 				case 5:
-					//System.out.println("Review selected");
 					JOptionPane.showMessageDialog(null,"Review selected");
 					initPass = false;
 					Review.main(conn, business_name, name_bool);
 					break;
 				case 6:
-					//System.out.println("User Compliments selected");
 					JOptionPane.showMessageDialog(null,"User Compliments selected");
 					initPass = false;
 					UserCompliments.main(conn, user_id, user_bool);
 					break;
 				case 7:
-					//System.out.println("Tip selected");
 					JOptionPane.showMessageDialog(null,"Tip selected");
 					initPass = false;
 					Tip.main(conn, business_name, name_bool);
 					break;
 				case 8:
-					//System.out.println("User selected");
 					JOptionPane.showMessageDialog(null,"User selected");
 					initPass = false;
 					User.main(conn, user_id, user_bool);
 					break;
 				case 9:
-					//System.out.println("Hours selected");
 					JOptionPane.showMessageDialog(null,"Hours selected");
 					initPass = false;
 					Hours.main(conn, business_name, name_bool);
 					break;
 				 default:
-					//System.out.println("Invalid argument");
 					JOptionPane.showMessageDialog(null,"Invalid argument");
 					initPass = false;
 					break;
 			}
 		}
-	   //create a statement object
-       //Statement stmt = conn.createStatement();
-       //create an SQL statement
-       //String sqlStatement = "SELECT \"Name\" FROM \"User\" LIMIT 10";
-       //send statement to DBMS
-       //ResultSet result = stmt.executeQuery(sqlStatement);
-
-       //OUTPUT
-       //System.out.println("Output");
-       //System.out.println("______________________________________");
-       //while (result.next()) {
-       //  System.out.println(result.getString("Name"));
-       //}
+		
    } catch (Exception e){
-     //System.out.println("Error accessing Database.");
 	 JOptionPane.showMessageDialog(null,"Error accessing Database.");
    }
     //closing the connection
     try {
       conn.close();
-      //System.out.println("Connection Closed.");
 	  JOptionPane.showMessageDialog(null,"Connection Closed.");
     } catch(Exception e) {
-      //System.out.println("Connection NOT Closed.");
 	  JOptionPane.showMessageDialog(null,"Connection NOT Closed.");
     }//end try catch
   }//end main
