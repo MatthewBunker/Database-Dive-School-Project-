68f32815a2205718d2f93c16fe8f982f752adbf5
emoryLu <qianlu@tamu.edu>
2020-09-30 16:24:35 -0500

    edited case 2 in switch statement for CheckIn.java

--- CheckIn.java
+++ CheckIn.java
@@ -7,17 +7,26 @@ import java.util.Date;
 public class CheckIn{
 	public static void main(Connection conn, String business_id, boolean id_bool) {
 		try {
+			System.out.println("0: Close");
+		   	System.out.println("1: I want to check in");
+		   	System.out.println("2: I want to see check in record for this business");
+
+		   	Scanner input = new Scanner(System.in);
+		   	System.out.println("Input desired command (enter \"0\" for Close): ");
+			int option = input.nextInt();
+
 			if (id_bool == true){
-				System.out.println("0: Close");
-		   		System.out.println("1: I want to check in");
-		   		System.out.println("2: I want to see check in record");
-
-		   		Scanner input = new Scanner(System.in);
-		   		System.out.println("Input desired command (enter \"0\" for Close): ");
-				int option = input.nextInt();
-
 				SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				System.out.println(format.format(new Date())); // test
 
+				/**
+				Statement stmt = conn.createStatement();
+				String sqlStatement = "SELECT * FROM \"CheckIn\" WHERE \"Business_ID\"=\'" + business_id + "\'";
+				ResultSet result = stmt.executeQuery(sqlStatement);	
+				System.out.println("can you see me");//test
+				**/
+
+				// when run, use business id: f9NumwFMBDn751xgFiRbNA
 				switch(option) {
 				case 0:
 					System.out.println("Close selected");
@@ -28,13 +37,19 @@ public class CheckIn{
 					// need to write this record to database
 					break;
 				case 2:
-					System.out.println("look for checkin record of " + business_id);
-					// need to print out record for business_name
+					System.out.println("Business_ID                | Date");
+					System.out.println("_______________________________");
+					//while(result.next()){
+						System.out.print(business_id);
+						System.out.print("    | ");
+						System.out.println(format.format(new Date()));
+					//}
 					break;
 				default:
 					System.out.println("Invalid argument");
 					break;
 				}
+				
 
 			} // end of if		
 		} 
