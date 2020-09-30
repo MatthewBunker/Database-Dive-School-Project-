9288ad3eb2d3465559828bcb4e4b5e1af0f247f0
emoryLu <qianlu@tamu.edu>
2020-09-30 17:30:23 -0500

    Converted to CheckIn.java to implement GUI. Note: this file is still in process.

--- CheckIn.java
+++ CheckIn.java
@@ -2,54 +2,73 @@ import java.sql.*;
 import java.util.Scanner;
 import java.text.SimpleDateFormat;  
 import java.util.Date;  
+import javax.swing.JOptionPane;
 
 //display parking record(s) of corresponding business(es)
 public class CheckIn{
 	public static void main(Connection conn, String business_id, boolean id_bool) {
 		try {
-			System.out.println("0: Close");
-		   	System.out.println("1: I want to check in");
-		   	System.out.println("2: I want to see check in record for this business");
+			String output = "";
+			String option = JOptionPane.showInputDialog(null,"0: Close\n"
+										    + "1: I want to check in\n"
+											+ "2: I want to see check in record for this business\n"
+											+ "Input desired command (enter \"0\" for Close): ");
 
-		   	Scanner input = new Scanner(System.in);
-		   	System.out.println("Input desired command (enter \"0\" for Close): ");
-			int option = input.nextInt();
-
-			if (id_bool == true){
-				SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-				System.out.println(format.format(new Date())); // test
-
-				/**
-				Statement stmt = conn.createStatement();
-				String sqlStatement = "SELECT * FROM \"CheckIn\" WHERE \"Business_ID\"=\'" + business_id + "\'";
-				ResultSet result = stmt.executeQuery(sqlStatement);	
-				System.out.println("can you see me");//test
-				**/
-
-				// when run, use business id: f9NumwFMBDn751xgFiRbNA
-				switch(option) {
-				case 0:
-					System.out.println("Close selected");
-					break;
-				case 1:
-					System.out.println("your checkin time is: ");
-					System.out.println(format.format(new Date()));
+			int optionInt = Integer.parseInt(option);
+			String option2 = null;
+			switch(optionInt) {
+			case 0:
+				option2 = "Close selected";
+				break;
+			case 1:
+					//System.out.println("your checkin time is: ");
+					//System.out.println(format.format(new Date()));
 					// need to write this record to database
-					break;
-				case 2:
+				option2 = "Check in";
+				break;
+			case 2:
+					/*
 					System.out.println("Business_ID                | Date");
 					System.out.println("_______________________________");
 					//while(result.next()){
-						System.out.print(business_id);
-						System.out.print("    | ");
-						System.out.println(format.format(new Date()));
+					System.out.print(business_id);
+					System.out.print("    | ");
+					System.out.println(format.format(new Date()));
 					//}
+					*/
+				option2 = "Obtain record";
+				break;
+			default:
+				break;
+			}
+
+			Statement stmt = conn.createStatement();
+			String sqlStatement = String.format("SELECT * FROM \"CheckIn\" WHERE \"Business_ID\"=\'" + business_id + " LIMIT 2",option2);
+			ResultSet result = stmt.executeQuery(sqlStatement);	
+
+			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			//System.out.println(format.format(new Date())); // test
+			// when run, use business id: f9NumwFMBDn751xgFiRbNA
+			switch(optionInt){
+				case 1:
+					/*
+					while (result.next()) {
+						output += result.getString("") + " | " + result.getString("Useful") + "\n";
+					}
+					*/
+					JOptionPane.showMessageDialog(null,"check in at: " +format.format(new Date()));
+					
+					break;
+				case 2:
+					while (result.next()) {
+						output += result.getString("Business_ID") + " | " + result.getString("Date") + "\n";
+					}
+					JOptionPane.showMessageDialog(null,"Business_ID | Date\n" + output);
 					break;
 				default:
-					System.out.println("Invalid argument");
+					JOptionPane.showMessageDialog(null,"Invalid Argument");
 					break;
-				}
-			} // end of if		
+			}
 		} 
 		catch (Exception e) {
 			System.out.println("Error accessing Database.");
