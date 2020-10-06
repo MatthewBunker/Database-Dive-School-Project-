5d588905ab0b712013fb2ff5436373999566eafa
aggiematthew99 <aggiematthew99@tamu.edu>
2020-10-06 18:31:39 -0500

    Implemented Pop Search Q3

--- Business.java
+++ Business.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 import javax.swing.JOptionPane;
 import java.util.HashMap;
 import java.util.Vector;
+import java.util.Collections;
 import java.util.Set;
+import java.awt.*;
+import java.util.ArrayList;
+import java.lang.*;
 
 public class Business{
 	public static void main(Connection conn, String business_name, String business_id, boolean name_bool) {
@@ -194,6 +198,7 @@ public class Business{
 		}
 	}
 
+
 	public static void shortest_chain_main(Connection conn, String business_1, String business_2){
 		//h
 	}
@@ -203,10 +208,12 @@ public class Business{
 			HashMap<String, Integer> business = new HashMap<String, Integer>();
 			Vector <String> business_vec = new Vector<String>();
 			Vector <String> franchises = new Vector<String>();
+			Vector<Double> lat = new Vector<Double>();
+			Vector<Double> longi = new Vector<Double>();
+			Vector<String> franchise_names = new Vector<String>();
 			String output = "";
 			Statement stmt = conn.createStatement();
 			String sqlStatement = String.format("SELECT * FROM \"business\" FULL JOIN \"Address\" ON \"Address\".\"Business_ID\"=\"business\".\"business_id\" WHERE \"Address\".\"state\"= \'" + state + "\' AND \"business\".\"rating\" >= 3.5");
-			System.out.println(sqlStatement);
 			ResultSet result = stmt.executeQuery(sqlStatement);
 
 			while(result.next()){
@@ -221,7 +228,6 @@ public class Business{
 					business.put(name, 1);
 				}
 				else{
-					//System.out.println("here");
 					business.put(name, ++count);
 				}
 			}
@@ -234,16 +240,82 @@ public class Business{
 			}
 
 			for(int i = 0; i < franchises.size(); i++){
-				//sqlstatement = String.format("dfd");
+				String company = franchises.elementAt(i);
+				StringBuffer sb = new StringBuffer(company);
+				for(int k = 0; k< company.length(); k++){
+					if(company.charAt(k) == '\''){
+						sb.insert(k + 1, "\'");
+					}
+				}
+				sqlStatement = String.format("SELECT \"Address\".\"state\", \"business\".\"Name\", \"business\".\"latitude\", \"business\".\"longitude\" FROM \"business\" FULL JOIN \"Address\" ON \"business\".\"business_id\"= \"Address\".\"Business_ID\" WHERE \"business\".\"Name\"= \'" + sb + "\' AND \"Address\".\"state\"= \'" + state + "\'");
+				result = stmt.executeQuery(sqlStatement);
+
+				while(result.next()){
+					//String temp = "Name: " + result.getString("Name") + " Latitude: " + result.getString("latitude") + " Longitude: " + result.getString("longitude") + "\n";
+					Double latitude = Double.parseDouble(result.getString("latitude"));
+					Double longitude = Double.parseDouble(result.getString("longitude"));
+					lat.add(latitude);
+					longi.add(longitude);
+					franchise_names.add(result.getString("Name"))	;	
+				}
 			}
 
-			//JOptionPane.showMessageDialog(null, output);
+			Vector<String> Top5 = new Vector<String>();
+			Top5 = maxDist(lat, longi, franchise_names);
+
+			for(int j = 0; j < 5; j++){
+				System.out.println(Top5.elementAt(j));
+			}		
 		}
 		catch(Exception e){
 			JOptionPane.showMessageDialog(null,"Error accessing Database.");
-		}
-		
+		}	
 	}
+
+	// Function calculates distance
+		// between two points
+		static Double dist(Double x1, Double x2, Double y1, Double y2){
+			Double x0 = x1 - x2;
+			Double y0 = y1 - y2;
+			Double ans = (x0 * x0) + (y0 * y0);
+			return ans;	
+		}
+	
+		// Function to find the maximum
+		// distance between any two points
+		static Vector<String> maxDist(Vector<Double> lat, Vector<Double> longi, Vector<String> franchise_names){
+			Vector<String> Top5 = new Vector<String>();
+			Vector<Double> Top5_dist = new Vector<Double>();
+		
+			Top5_dist.add(0, 0.0);
+			Top5_dist.add(0, 0.0);
+			Top5_dist.add(0, 0.0);
+			Top5_dist.add(0, 0.0);
+			Top5_dist.add(0, 0.0);
+
+			Top5.add(0, "temp");
+			Top5.add(0, "temp");
+			Top5.add(0, "temp");
+			Top5.add(0, "temp");
+			Top5.add(0, "temp");
+	
+			// Iterate over all possible pairs
+			for (int i = 0; i < lat.size() - 1; i++) {
+				if(franchise_names.elementAt(i).equals(franchise_names.elementAt(i+1))){
+					Double distance = dist(lat.elementAt(i), lat.elementAt(i+1),longi.elementAt(i), longi.elementAt(i+1));
+					Double min = Collections.min(Top5_dist);
+					if(distance > min && !Top5.contains(franchise_names.elementAt(i))){
+						for(int j = 0; j < 5; j++){
+							if(Top5_dist.elementAt(j) == min){
+								Top5_dist.set(j, distance);
+								Top5.set(j, franchise_names.elementAt(i));
+							}
+						}
+					}
+				}
+			}
+			return Top5;
+		}
 
 	public static void local_restaurant_main(Connection conn, String city){
 		//h
