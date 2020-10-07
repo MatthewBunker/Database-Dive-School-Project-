6fd9958ae583de3c31d0d92e98089f1d6aa0e59d
aggiematthew99 <aggiematthew99@tamu.edu>
2020-10-06 21:21:07 -0500

    deleted main for alvin to work on and started Q4

--- Business.java
+++ Business.java
@@ -11,191 +11,7 @@ import java.lang.*;
 
 public class Business{
 	public static void main(Connection conn, String business_name, String business_id, boolean name_bool) {
-		try{
-			String output = "";
-			String txtoption = "";
-			Scanner input = new Scanner(System.in);
-			if(name_bool == true){
-				txtoption = JOptionPane.showInputDialog(null, "1: Categories for: " + business_name + "\n"
-														+ "2: Take out for: " + business_name + "\n"
-														+ "3: Rating for: " + business_name + "\n"
-														+ "4: Business ID for: " + business_name);
-				/*System.out.println("1: Categories for: " + business_name);
-				System.out.println("2: Take out for: " + business_name);
-				System.out.println("3: Rating for: " + business_name);
-				System.out.println("4: Business ID for: " + business_name);*/
-			}
-			else{
-				txtoption = JOptionPane.showInputDialog(null, "1: Categories for first 10 companies \n" 
-															+ "2: Take out for first 10 companies\n"
-															+ "3: Rating for first 10 companies\n"
-															+ "4: Business ID for first 10 companies\n"
-															+ "5: Name of first 10 companies");
-				/*System.out.println("1: Categories for first 10 companies ");
-				System.out.println("2: Take out for first 10 companies");
-				System.out.println("3: Rating for first 10 companies");
-				System.out.println("4: Business ID for first 10 companies");
-				System.out.println("5: Name of first 10 companies");*/
-			}
-			int option = Integer.parseInt(txtoption);
-
-			//create a statement object
-			Statement stmt = conn.createStatement();
-
-			if(name_bool == true){
-				String sqlStatement = "SELECT * FROM \"business\" WHERE \"Name\"=\'" + business_name + "\'";
-				ResultSet result = stmt.executeQuery(sqlStatement);	
-
-				switch(option){
-					case 1:
-						output += "Business Name                | Category\n" +
-						"_______________________________\n";
-						//System.out.println("Business Name                | Category");
-						//System.out.println("_______________________________");
-
-						while(result.next()){
-							output += result.getString("Name") + " | " + result.getString("category") + "\n"; 
-							/*System.out.print(result.getString("Name"));
-							System.out.print(" | ");
-							System.out.println(result.getString("category"));*/
-						}
-						JOptionPane.showMessageDialog(null, output);
-						break;
-					case 2:
-						output += "Business Name                | Take out\n" +
-						"_______________________________\n";
-						//System.out.println("Business Name                | Take out");
-						//System.out.println("_______________________________");
-
-						while(result.next()){
-							output += result.getString("Name") + " | " + result.getString("take out ") + "\n";
-							//System.out.print(result.getString("Name"));
-							//System.out.print(" | ");
-							//take out has invisible space at end of it
-							//System.out.println(result.getString("take out "));
-						}
-						JOptionPane.showMessageDialog(null, output);
-						break;
-					case 3:
-						output += "Business Name                | Rating\n" +
-						"_______________________________\n";
-						//System.out.println("Business Name                | Rating");
-						//System.out.println("_______________________________");
-
-						while(result.next()){
-							output += result.getString("Name") + " | " + result.getString("rating") + "\n";
-							/*System.out.print(result.getString("Name"));
-							System.out.print(" | ");
-							System.out.println(result.getString("rating"));*/
-						}
-						JOptionPane.showMessageDialog(null, output);
-						break;
-					case 4:
-						output += "Business Name                | Business ID\n" +
-						"_______________________________\n";
-						//System.out.println("Business Name                | Business ID");
-						//System.out.println("_______________________________");
-
-						while(result.next()){
-							output += result.getString("Name") + " | " + result.getString("business_id") + "\n";
-							/*System.out.print(result.getString("Name"));
-							System.out.print(" | ");
-							System.out.println(result.getString("business_id"));*/
-						}
-						JOptionPane.showMessageDialog(null, output);
-						break;
-					default:
-						output += "Invalid Argument: Closing\n";
-						//System.out.println("Invalid Argument: Closing");
-						JOptionPane.showMessageDialog(null, output);
-						break;
-				}
-			}
-			else{
-				String sqlStatement = "SELECT * FROM \"business\" LIMIT 10";
-				ResultSet result = stmt.executeQuery(sqlStatement);	
-
-				switch(option){
-					case 1:
-						output += "Business Name                | Category\n" +
-						"_______________________________\n";
-						//System.out.println("Business Name                | Category");
-						//System.out.println("_______________________________");
-
-						while(result.next()){
-							output += result.getString("Name") + " | " + result.getString("category") + "\n"; 
-							/*System.out.print(result.getString("Name"));
-							System.out.print(" | ");
-							System.out.println(result.getString("category"));*/
-						}
-						JOptionPane.showMessageDialog(null, output);
-						break;
-					case 2:
-						output += "Business Name                | Take out\n" +
-						"_______________________________\n";
-						//System.out.println("Business Name                | Take out");
-						//System.out.println("_______________________________");
-
-						while(result.next()){
-							output += result.getString("Name") + " | " + result.getString("take out ") + "\n";
-							//System.out.print(result.getString("Name"));
-							//System.out.print(" | ");
-							//take out has invisible space at end of it
-							//System.out.println(result.getString("take out "));
-						}
-						JOptionPane.showMessageDialog(null, output);
-						break;
-					case 3:
-						output += "Business Name                | Rating\n" +
-						"_______________________________\n";
-						//System.out.println("Business Name                | Rating");
-						//System.out.println("_______________________________");
-
-						while(result.next()){
-							output += result.getString("Name") + " | " + result.getString("rating") + "\n";
-							/*System.out.print(result.getString("Name"));
-							System.out.print(" | ");
-							System.out.println(result.getString("rating"));*/
-						}
-						JOptionPane.showMessageDialog(null, output);
-						break;
-					case 4:
-						output += "Business Name                | Business ID\n" +
-						"_______________________________\n";
-						//System.out.println("Business Name                | Business ID");
-						//System.out.println("_______________________________");
-
-						while(result.next()){
-							output += result.getString("Name") + " | " + result.getString("business_id") + "\n";
-							/*System.out.print(result.getString("Name"));
-							System.out.print(" | ");
-							System.out.println(result.getString("business_id"));*/
-						}
-						JOptionPane.showMessageDialog(null, output);
-						break;
-					case 5:
-						output += "Business Name\n_______________________________\n";
-						//System.out.println("Business Name");
-						//System.out.println("_______________________________");
-
-						while(result.next()){
-							output += result.getString("Name") + "\n";
-							//System.out.println(result.getString("Name"));
-						}
-						JOptionPane.showMessageDialog(null, output);
-						break;
-					default:
-						output += "Invalid Argument: Closing\n";
-						//System.out.println("Invalid Argument: Closing");
-						JOptionPane.showMessageDialog(null, output);
-						break;
-				}
-			}				
-		}
-		catch(Exception e){
-			JOptionPane.showMessageDialog(null,"Error accessing Database.");
-			//System.out.println("Error accessing Database.");
-		}
+	
 	}
 
 
@@ -213,7 +29,7 @@ public class Business{
 			Vector<String> franchise_names = new Vector<String>();
 			String output = "";
 			Statement stmt = conn.createStatement();
-			String sqlStatement = String.format("SELECT * FROM \"business\" FULL JOIN \"Address\" ON \"Address\".\"Business_ID\"=\"business\".\"business_id\" WHERE \"Address\".\"state\"= \'" + state + "\' AND \"business\".\"rating\" >= 3.5");
+			String sqlStatement = String.format("SELECT \"business\".\"Name\" FROM \"business\" FULL JOIN \"Address\" ON \"Address\".\"Business_ID\"=\"business\".\"business_id\" WHERE \"Address\".\"state\"= \'" + state + "\' AND \"business\".\"rating\" >= 3.5");
 			ResultSet result = stmt.executeQuery(sqlStatement);
 
 			while(result.next()){
@@ -247,11 +63,10 @@ public class Business{
 						sb.insert(k + 1, "\'");
 					}
 				}
-				sqlStatement = String.format("SELECT \"Address\".\"state\", \"business\".\"Name\", \"business\".\"latitude\", \"business\".\"longitude\" FROM \"business\" FULL JOIN \"Address\" ON \"business\".\"business_id\"= \"Address\".\"Business_ID\" WHERE \"business\".\"Name\"= \'" + sb + "\' AND \"Address\".\"state\"= \'" + state + "\'");
+				sqlStatement = String.format("SELECT \"business\".\"Name\", \"business\".\"latitude\", \"business\".\"longitude\" FROM \"business\" FULL JOIN \"Address\" ON \"business\".\"business_id\"= \"Address\".\"Business_ID\" WHERE \"business\".\"Name\"= \'" + sb + "\' AND \"Address\".\"state\"= \'" + state + "\'");
 				result = stmt.executeQuery(sqlStatement);
 
 				while(result.next()){
-					//String temp = "Name: " + result.getString("Name") + " Latitude: " + result.getString("latitude") + " Longitude: " + result.getString("longitude") + "\n";
 					Double latitude = Double.parseDouble(result.getString("latitude"));
 					Double longitude = Double.parseDouble(result.getString("longitude"));
 					lat.add(latitude);
@@ -263,9 +78,11 @@ public class Business{
 			Vector<String> Top5 = new Vector<String>();
 			Top5 = maxDist(lat, longi, franchise_names);
 
+			output = "Top 5 Business with furthest location spread and rating over 3.5\n" + "--------------------------------------------------\n";
 			for(int j = 0; j < 5; j++){
-				System.out.println(Top5.elementAt(j));
-			}		
+				output += Top5.elementAt(j) + "\n";
+			}
+			JOptionPane.showMessageDialog(null, output);		
 		}
 		catch(Exception e){
 			JOptionPane.showMessageDialog(null,"Error accessing Database.");
@@ -318,7 +135,75 @@ public class Business{
 		}
 
 	public static void local_restaurant_main(Connection conn, String city){
-		//h
+		try{
+			HashMap<String, Integer> business = new HashMap<String, Integer>();
+			Vector <String> business_vec = new Vector<String>();
+			Vector <String> business_id = new Vector<String>();
+			Vector <String> local = new Vector<String>();
+			String output = "";
+			Statement stmt = conn.createStatement();
+			String sqlStatement = String.format("SELECT \"business\".\"Name\" FROM \"business\" FULL JOIN \"Address\" ON \"Address\".\"Business_ID\"=\"business\".\"business_id\" WHERE \"Address\".\"city\"= \'" + city + "\'");
+			ResultSet result = stmt.executeQuery(sqlStatement);
+
+			while(result.next()){
+				String temp = result.getString("Name");
+				business_vec.add(temp);
+			}
+
+
+			for(String name : business_vec){
+				Integer count = business.get(name);
+				if(count == null){
+					business.put(name, 1);
+				}
+				else{
+					business.put(name, ++count);
+				}
+			}
+
+			for(String name : business.keySet()){
+				Integer count = business.get(name);
+				if(count == 1){
+					local.add(name);
+				}
+			}
+
+			for(int i = 0; i < local.size(); i++){
+				String company = local.elementAt(i);
+				StringBuffer sb = new StringBuffer(company);
+				for(int k = 0; k< company.length(); k++){
+					if(company.charAt(k) == '\''){
+						sb.insert(k + 1, "\'");
+					}
+				}
+
+				sqlStatement = String.format("SELECT \"business\".\"business_id\" FROM \"business\" WHERE \"business\".\"Name\"= \'" + sb + "\'");
+				result = stmt.executeQuery(sqlStatement);
+
+				while(result.next()){
+					business_id.add(result.getString("business_id"));
+				}
+			}
+
+			int Max_count = 0;
+			int tip_count = 0;
+			String best_local = "temp";
+			for(int i = 0; i < business_id.size(); i++){
+				sqlStatement = String.format("SELECT COUNT(\"tip\".\"Business_ID\"), \"tip\".\"Business_ID\"  FROM \"tip\" WHERE \"tip\".\"Business_ID\"= \'" + business_id.elementAt(i) + "\'");
+				System.out.println(sqlStatement);
+				result = stmt.executeQuery(sqlStatement);
+
+				tip_count = Integer.parseInt(result.getString("count"));
+				if(tip_count > Max_count){
+					Max_count = tip_count;
+					best_local = result.getString("Business_ID");
+				}
+			}
+			System.out.println(best_local + " " + Max_count);
+		}
+		catch(Exception e){
+			JOptionPane.showMessageDialog(null,"Error accessing Database.");
+		}
 	}
 
 	public static void rated_business_main(Connection conn, String category, String city){
